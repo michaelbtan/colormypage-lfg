e9faa8ec99eecc4f12ae76c25bbf3b04790e21ce
Michael Tan <tanmichael74@yahoo.com>
2025-06-01 17:35:47 -0400

    nextconfig ignores

--- colormypage/next.config.ts
+++ colormypage/next.config.ts
@@ -9,18 +9,18 @@ const nextConfig: NextConfig = {
       "images.unsplash.com"
     ],
   },
-  // eslint: {
-  //   // Warning: This allows production builds to successfully complete even if
-  //   // your project has ESLint errors.
-  //   ignoreDuringBuilds: true,
-  // },
-  // typescript: {
-  //   // !! WARN !!
-  //   // Dangerously allow production builds to successfully complete even if
-  //   // your project has type errors.
-  //   // !! WARN !!
-  //   ignoreBuildErrors: true,
-  // },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig
